12b387480ca10bbb631ac204f2f10234a7f8e280
Bankad1 <bankad123@gmail.com>
2021-05-06 15:28:15 +0200

    SzamlaOsztaly

---
+++ BeadandoBD/BeadandoBD/Szamlak.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeadandoBD
+{
+    /// <summary>
+    /// Számla osztály és adattagjai
+    /// </summary>
+    class Szamlak
+    {
+        public int Id { get; set; }
+        public string Tulajdonos { get; set; }
+        public int Egyenleg { get; set; }
+
+        public Szamlak(int id, string tulajdonos, int egyenleg)
+        {
+            Id = id;
+            Tulajdonos = tulajdonos;
+            Egyenleg = egyenleg;
+        }
+    }
+}
